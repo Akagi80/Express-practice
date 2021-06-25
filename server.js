88beef5292916b44e57e5a165615b2f205df7d07
Sebastian Sikora <ssikora80@gmail.com>
2021-06-25 13:56:16 +0200

    Add new endpoint, first request, contact from and styles

--- server.js
+++ server.js
@@ -10,6 +10,20 @@ app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(express.urlencoded({ extended: false })); //obsługa formularzy x-www-form-urlencoded
+
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message) {
+    res.render('contact', { isSent: true });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+});
+
 app.get('/', (req, res) => {
   res.render('index');
 });
